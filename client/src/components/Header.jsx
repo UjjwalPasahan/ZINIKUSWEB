0df09d911a467235ef909bdb9f6650fbc3ed4273
UjjwalPasahan <96993068+UjjwalPasahan@users.noreply.github.com>
2023-06-15 15:34:33 +0530

    home and header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link ,HashLink } from 'react-router-dom';
 import { GiHamburgerMenu } from 'react-icons/gi';
+import { ImSearch } from 'react-icons/im';
 import {
   Box,
   VStack,
@@ -12,10 +14,22 @@ import {
   DrawerHeader,
   DrawerOverlay,
   DrawerBody,
+  Input,
+  useBoolean,
+  Image,
+  Stack
 } from '@chakra-ui/react';
+import img2 from '../assets/homepage/zinikus.png'
+import '../styles/header.css'
 
 const Header = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const [showInput,setShowInput] = useState(false);
+
+  const handleClick = ()=>{
+    setShowInput(true);
+    setShowInput(!showInput);
+  }
 
   return (
     <div>
@@ -31,13 +45,35 @@ const Header = () => {
         <GiHamburgerMenu />
       </Button>
 
-      <Drawer isOpen={isOpen} onClose={onClose} placement="left">
+      <Drawer isOpen={isOpen} onClose={onClose} placement="left" >
         <DrawerOverlay />
         <DrawerContent>
           <DrawerCloseButton onClick={onClose} />
-          <DrawerHeader></DrawerHeader>
+          <DrawerHeader h='container.sm'>
+          <div>
+            <Image src={img2} />
+            <Button onClick={handleClick} colorScheme='facebook'> <ImSearch onClick={!handleClick}/> </Button>
+            {showInput && <Input placeholder='Type here...' colorScheme='facebook'/>}
+          </div>
+          </DrawerHeader>
+          <VStack className='links'>
+            <Button  onClick={onClose} w='40' colorScheme='facebook'><Link to='#sevices'>Our Services</Link></Button>
+            <Button onClick={onClose}  w='40' colorScheme='facebook'><Link to='#team'>Our Team</Link></Button>
+            <Button onClick={onClose}  w='40' colorScheme='facebook'><Link to='#product'>Products</Link></Button>
+            <Button onClick={onClose}  w='40' colorScheme='facebook'><Link to='#contact'>Contact Us</Link></Button>
+          </VStack>
+
+          <Stack direction={["column","row"]} className='ls' justifyContent='space-around'>
+            <Button onClick={onClose}  w='20' colorScheme='facebook'><Link to='#login'>Login</Link></Button>
+            <Button onClick={onClose}  w='20' colorScheme='facebook' variant='outline'><Link to='#signup'>SignUp</Link></Button>
+          </Stack>
         </DrawerContent>
       </Drawer>
+
+      <div><Image src={img2} pos='fixed' top='0.3' left={['23%','40%']} h={['12','110']}/></div>
+
+
+
     </div>
   );
 };
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import img from '../assets/homepage/Subtract.png'
 import { Box, Image } from '@chakra-ui/react'
+import '../styles/home.css'
 
 const Home = () => {
   return (
     
-    <Box w='full'>
-        <Image w='full' src={img} />
-    </Box>
+    <Box
+  backgroundImage={img} 
+  backgroundSize="cover"
+  w="100%" 
+  h="85rem">
+  
+</Box>
   )
 }
 
