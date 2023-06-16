3a245448797c33650c3fcd4c997a34c9f4c6b2a3
utkarsh-gaur <utkarshgaur6014@gmail.com>
2023-06-16 16:11:05 +0530

    some more changes

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-
+import '../styles/Footer.css'
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div >Footer</div>
   )
 }
 
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import img from '../assets/homepage/Subtract.png'
-import { Box, Image, VStack,HStack, Button, flexbox, Center } from '@chakra-ui/react'
+import headImg from '../assets/homepage/Subtract.png'
+import { Box, Image, VStack, HStack, Button, flexbox, Center } from '@chakra-ui/react'
 import '../styles/home.css'
 import { Link } from 'react-router-dom'
 import img3 from '../assets/homepage/bg.png'
@@ -8,27 +8,31 @@ import About from './About'
 
 const Home = () => {
   return (
-   <div className='home'> 
-    <Box
-  backgroundImage={img} 
-  backgroundSize='cover'
-  display='flex'
-  justifyContent='center'
-  backgroundRepeat={'no-repeat'}
-  w="100%" 
-  h="65rem">
+    <div className='gradient'>
+      <div className='home'>
+        <Box className='headImage'
+          // backgroundImage={headImg}
+          // backgroundSize='cover'
+          // display='flex'
+          // zIndex='-150'
+          // justifyContent='center'
+          // backgroundRepeat={'no-repeat'}
+          // w="100%"
+          // h="65rem"
+          >
 
-  <VStack pos='absolute' top={['32','72']} left={['15','60']}>
-    <div className='hometext'>
-      <h2>Translation by <br/> H.Rackham</h2>
-      <h5>Sit nostrud officia eu adipisicing<br/> occaecat voluptate labore.</h5>
-      <Button colorScheme='facebook' borderRadius='3xl' variant='outline' h='16' w='36' fontSize='3xl'><Link to='#login'>Login</Link></Button>
-    </div>
-  </VStack>
-  
-</Box>
-  <About/>
- </div> )
+          <VStack pos='absolute' top={['32', '72']} left={['15', '60']}>
+            <div className='hometext'>
+              <h2>Translation by <br /> H.Rackham</h2>
+              <h5>Sit nostrud officia eu adipisicing<br /> occaecat voluptate labore.</h5>
+              <Button colorScheme='facebook' borderRadius='3xl' variant='outline' h='16' w='36' fontSize='3xl'><Link to='#login'>Login</Link></Button>
+            </div>
+          </VStack>
+
+        </Box>
+        <About />
+      </div>
+    </div>)
 }
 
 export default Home
