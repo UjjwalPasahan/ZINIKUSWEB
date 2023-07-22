9738415d7d1fc3914c7dba9e40473105b4475741
utkarsh-gaur <utkarshgaur6014@gmail.com>
2023-07-23 02:06:16 +0530

    last

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,7 +4,12 @@ const Footer = () => {
   return (
     <div className='footer' >
       <hr className='defaultHR'/>
-      <div class = "vertical"></div>
+      
+      <div className='vertical'>
+      <div class = "vertical1"></div>
+      <div class = "vertical2"></div>
+      </div>
+
       </div>
 
   )
