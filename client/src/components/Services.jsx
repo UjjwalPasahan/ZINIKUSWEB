3e2490585970924153e01de99255b2824e455c08
UjjwalPasahan <96993068+UjjwalPasahan@users.noreply.github.com>
2023-07-24 13:52:32 +0530

    CHANGES 2

--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
+import '../styles/services.css'
 const Services = () => {
   return (
-    <div>Services</div>
+    <div className='imageCalling'>
+      <div>
+        uasgdhjask
+      </div>
+    </div>
   )
 }
 
-export default Services
+export default Services;
