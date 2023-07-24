8be5548425ab2d7aa5c5a7a943d6cfe9d72fc6bc
utkarsh-gaur <utkarshgaur6014@gmail.com>
2023-07-24 13:51:04 +0530

    changes

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -8,17 +8,17 @@ const Footer = () => {
         <div class="contact">Contact</div>
         <div class="products">Products</div>
         <div class="address">Address</div>
-        <div class="feedback">feedback</div>
-        <div class="newsletter">newsletter</div>
+        <div class="feedback">Feedback</div>
+        <div class="newsletter">Our Newsletter</div>
         <div class="email">email</div>
-        <div class="footerAbout">About</div>
+        <div class="footerAbout">About Us</div>
         <div class="terms">Terms & Conditions</div>
         <div class="phone">Phone Number</div>
       </div>
-      {/* <div className='vertical'>
+      <div className='vertical'>
       <div class = "vertical1"></div>
       <div class = "vertical2"></div>
-      </div> */}
+      </div>
 
       </div>
 
