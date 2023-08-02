f759ed0b2871645421b67512fca5f52c5080c09f
utkarsh-gaur <utkarshgaur6014@gmail.com>
2023-08-02 23:11:44 +0530

    Revert "fixes"
    This reverts commit ed380099681c1e87405c059382b7de33d52a0659.

--- client/src/components/Team.jsx
+++ client/src/components/Team.jsx
@@ -4,38 +4,12 @@ import '../styles/team.css';
 const Team = props => {
   return (
     <div className="wrapper">
+      
       <div className="teams">
-        <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
-          <div className="carousel-inner">
-            <div className="carousel-item active">
-              <img className="teamimg" src={props.image} alt={props.name} />
-              <h2 className="teamh2">{props.name}</h2>
-              <h3 className="teamh3">{props.working_area}</h3>
-              <p className="teamp">{props.description}</p>
-            </div>
-            <div className="carousel-item">
-              <img className="teamimg" src={props.image} alt={props.name} />
-              <h2 className="teamh2">{props.name}</h2>
-              <h3 className="teamh3">{props.working_area}</h3>
-              <p className="teamp">{props.description}</p>
-            </div>
-            <div className="carousel-item">
-              <img className="teamimg" src={props.image} alt={props.name} />
-              <h2 className="teamh2">{props.name}</h2>
-              <h3 className="teamh3">{props.working_area}</h3>
-              <p className="teamp">{props.description}</p>
-            </div>
-          </div>
-          <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span className="visually-hidden">Previous</span>
-          </button>
-          <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-            <span className="visually-hidden">Next</span>
-          </button>
-        </div>
-
+        <img className="teamimg" src={props.image} />
+        <h2 className="teamh2">{props.name}</h2>
+        <h3 className="teamh3">{props.working_area}</h3>
+        <p className="teamp">{props.description}</p>
       </div>
     </div>
   );
