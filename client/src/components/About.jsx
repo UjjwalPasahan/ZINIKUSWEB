6c0ff16df9495546248bde9fa6c6e8e1a626b5b1
UjjwalPasahan <96993068+UjjwalPasahan@users.noreply.github.com>
2023-08-06 13:47:54 +0530

    about us and header changes

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -72,34 +72,15 @@ const About = () => {
       <div className='upperAbout'>
       {/* <div className='abouth2'><h2>ABOUT US</h2></div> */}
       <div className='h22'><h2>ABOUT US</h2></div>
+      <div className='lowerSpaceMaker' ></div>
       <div><p className='aboutp' >
-        Amet incididunt Lorem sunt cupidatat fugiat fugiat amet mollit laborum
-        minim eiusmod. Veniam eu exercitation sunt voluptate consequat nisi
-        dolore. Excepteur reprehenderit labore amet occaecat eu aliquip labore.
-        Anim anim minim veniam eu fugiat. Irure sunt ullamco mollit ipsum ipsum
-        officia exercitation veniam consequat anim dolore. Et adipisicing non
-        labore dolore ea do do. Incididunt nisi laborum ipsum Lorem eu do
-        proident sit excepteur fugiat incididunt ut. Voluptate enim irure ipsum
-        et labore do enim Lorem qui aliquip officia esse ipsum. Quis elit
-        consequat occaecat nostrud irure culpa dolore. Adipisicing reprehenderit
-        consectetur est do nulla minim quis esse veniam. Aute labore dolore
-        ipsum nulla laboris magna ea. Sit ea officia aliquip dolore eiusmod
-        occaecat incididunt occaecat. Consectetur et qui est nisi fugiat
-        adipisicing sint aliqua amet excepteur adipisicing. Excepteur ipsum in
-        laborum nisi excepteur eiusmod eiusmod culpa eu. Dolore ea Lorem sint
-        proident excepteur ullamco labore ex. Pariatur laboris ex duis sunt in
-        cupidatat adipisicing adipisicing aliqua culpa mollit duis. Consectetur
-        nisi incididunt id Lorem pariatur occaecat adipisicing adipisicing duis
-        labore ex nulla voluptate sunt. Excepteur nostrud exercitation enim
-        officia amet excepteur ullamco labore exercitation pariatur. Fugiat
-        magna laborum dolor officia. Laboris ad quis cillum aliqua aliqua quis
-        dolore. Ipsum velit aute dolor enim adipisicing magna culpa ipsum minim
-        dolor elit velit ullamco. Culpa ex cillum non ut est. Minim nisi sint
-        aliquip fugiat adipisicing. Excepteur pariatur incididunt non aliqua
-        irure fugiat sunt aute. Ex deserunt cillum cupidatat ad. Eu non aute
-        nostrud reprehenderit velit adipisicing ex culpa qui. Quis dolore tempor
-        ipsum sint ex est aliquip ullamco. Adipisicing non eu dolore irure quis
-        nostrud nostrud labore esse in anim esse.
+      Finding Inspiration in Every Turn
+<br/>
+We are dedicated to creating cutting-edge technology that pushes the boundaries of what's possible in the world of automation. Our team is composed of experts in various fields, including robotics, machine learning, computer science, and engineering. Together, we're creating a future where robots can perform complex tasks with ease, making our lives easier and more efficient.
+
+<br/>
+
+At our company, we believe in constant innovation and collaboration. We're always looking for new ways to improve our products and services and are open to working with partners who share our vision. We're excited to be at the forefront of this exciting field and look forward to pushing the boundaries of what's possible in the world of A.I. and robotics.
       </p></div>
       </div>
       <HStack className='lowerAboutUs' mt="40">
--- client/src/components/HeaderContent.jsx
+++ client/src/components/HeaderContent.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import {
   Box,
   VStack,
@@ -18,7 +18,7 @@ import {
   Stack,
 } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
-import { GiHamburgerMenu } from "react-icons/gi";
+import { GiHamburgerMenu } from 'react-icons/gi';
 import { ImSearch } from 'react-icons/im';
 import img2 from '../assets/homepage/zinikus.png';
 import '../styles/header.css';
@@ -35,7 +35,7 @@ const HeaderContent = () => {
 
   return (
     <>
-      <div className='headerContent'>
+      <div className="headerContent">
         <HStack className="links gap" w="100%">
           {/* <Button onClick={onClose} w="40" className='linkbtn' colorScheme="gray" variant={'ghost'}>
       <Link to={'/services'}>Our Services</Link>
@@ -47,56 +47,76 @@ const HeaderContent = () => {
       <Link to={'/research'}>Research</Link>
     </Button>  */}
           {/* <!--start button, nothing above this is necessary --> */}
-          <div className="svg-wrapper">
-            <div id="text">
-              <a href=""><span height="40" width="150" className="spot">Services</span></a>
-            </div>
-            <svg height="40" width="150" xmlns="http://www.w3.org/2000/svg">
-              <rect id="shape" height="40" width="150" />
-            </svg>
+          <div className="svg-wrapper" style={{ marginLeft: '3.7vw' }}>
+            <Link to={'/services'}>
+              <div id="text">
+                <a href="/services"><span height="40" width="150" className="spot">Services</span></a>
+              </div>
+              <svg height="40" width="150" xmlns="http://www.w3.org/2000/svg">
+                <rect id="shape" height="40" width="150" />
+              </svg>
+            </Link>
           </div>
           {/* <!--Next button --> */}
           <div className="svg-wrapper">
-            <div id="text">
-              <a href=""><span height="40" width="150" className="spot">Products</span></a>
-            </div>
-            <svg height="40" width="150" xmlns="http://www.w3.org/2000/svg">
-              <rect id="shape" height="40" width="150" />
-            </svg>
+            <Link to={'/product'}>
+              <div id="text">
+                <a href=""><span height="40" width="150" className="spot">Products</span></a>
+              </div>
+              <svg height="40" width="150" xmlns="http://www.w3.org/2000/svg">
+                <rect id="shape" height="40" width="150" />
+              </svg>
+            </Link>
           </div>
           {/* <!--Next button --> */}
           <div className="svg-wrapper">
-            <div id="text">
-              <a href=""><span height="40" width="150" className="spot">Research</span></a>
-            </div>
-            <svg height="40" width="150" xmlns="http://www.w3.org/2000/svg">
-              <rect id="shape" height="40" width="150" />
-            </svg>
+            <Link to={'/research'}>
+              <div id="text">
+                <a href="/research"><span height="40" width="150" className="spot">Research</span></a>
+              </div>
+              <svg height="40" width="150" xmlns="http://www.w3.org/2000/svg">
+                <rect id="shape" height="40" width="150" />
+              </svg>
+            </Link>
           </div>
           {/* <!--End button --> */}
-          <div className="svg-wrapper">
-            <div id="text textNews">
+          {/* <div className="svg-wrapper">
+            <div id="text">
               <a href=""><span height="40" width="150" className="spot">Newsletter</span></a>
             </div>
             <svg height="40" width="150" xmlns="http://www.w3.org/2000/svg">
-              <rect id="shape shapeNews" height="40" width="150" />
+              <rect id="shape " height="40" width="150" />
             </svg>
-          </div>
-          {/* <Button onClick={onClose} w="40" className='linkbtn newsbtn' colorScheme="facebook" variant={'unstyled'}>
-            <Link to={'/newsletter'} className='btnborder'>Newsletter</Link>
-          </Button> */}
+          </div> */}
+          <Button
+            onClick={onClose}
+            w="40"
+            className="linkbtn newsbtn"
+            colorScheme="facebook"
+            variant={'unstyled'}
+          >
+            <Link to={'/newsletter'} className="btnborder">
+              Newsletter
+            </Link>
+          </Button>
         </HStack>
-
-
       </div>
       <div className="flex">
         <div>
-          <Link to={'/'}><Image src={img2} className="mainLogo" pos={'absolute'} top={'0.5'} left={'3%'} /></Link>
+          <Link to={'/'}>
+            <Image
+              src={img2}
+              className="mainLogo"
+              pos={'absolute'}
+              top={'1'}
+              left={'3%'}
+            />
+          </Link>
         </div>
       </div>
       <hr className="headerHR" />
     </>
-  )
-}
+  );
+};
 
-export default HeaderContent
+export default HeaderContent;
