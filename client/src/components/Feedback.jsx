b06c7e2e13c50148ae26ca2b415559c31f7c5ae5
UjjwalPasahan <96993068+UjjwalPasahan@users.noreply.github.com>
2023-08-07 17:56:41 +0530

    feedback and tnc enhanced

--- client/src/components/Feedback.jsx
+++ client/src/components/Feedback.jsx
@@ -3,8 +3,18 @@ import { Button, Input, VStack } from '@chakra-ui/react';
 import '../styles/feedback.css';
 import { MdKeyboardBackspace } from 'react-icons/md';
 import { Link } from 'react-router-dom';
+import {StarIcon} from '@chakra-ui/icons'
+import { useState, useEffect } from 'react';
 
 const Feedback = () => {
+  
+  const [rating, setRating] = useState(0);
+
+  // Function to update the rating and store it in local storage
+  const handleStarClick = (selectedRating) => {
+    setRating(selectedRating);
+    localStorage.setItem('rating', selectedRating);
+  };
   return (
     <div className="backgroundimg">
       <div className="back">
@@ -42,20 +52,37 @@ const Feedback = () => {
           />
           <Input
             variant="flushed"
-            placeholder="Subject"
+            placeholder="What did you like the best?"
             type='text'
             colorScheme="facebook"
             required
           />
           <Input
             variant="flushed"
-            placeholder="Message"
+            placeholder="How can we improve?"
             type='textbox'
             colorScheme="facebook"
             required
           />
 
-          <Button colorScheme="twitter" m={'10'} variant={'solid'}>
+<div>
+      {[...Array(5)].map((_, index) => (
+        <span
+          key={index}
+          style={{
+            cursor: 'pointer',
+            fontSize: '24px',
+            color: index < rating ? '#2b00ff' : 'black',
+          }}
+          onClick={() => handleStarClick(index + 1)}
+        >
+          ★
+        </span>
+      ))}
+    </div>
+
+
+          <Button colorScheme="twitter" m={'10px'} variant={'solid'}>
             Submit
           </Button>
         </VStack>
@@ -64,4 +91,6 @@ const Feedback = () => {
   );
 };
 
-export default Feedback;
+
+export default Feedback
+
--- client/src/components/termsConditions.jsx
+++ client/src/components/termsConditions.jsx
@@ -10,33 +10,53 @@ const termsConditions = () => {
     <Header/>
       <div className='termsContainer'>
         <div className='termsText'>
-        Amet incididunt Lorem sunt cupidatat fugiat fugiat amet mollit laborum
-        minim eiusmod. Veniam eu exercitation sunt voluptate consequat nisi
-        dolore. Excepteur reprehenderit labore amet occaecat eu aliquip labore.
-        Anim anim minim veniam eu fugiat. Irure sunt ullamco mollit ipsum ipsum
-        officia exercitation veniam consequat anim dolore. Et adipisicing non
-        labore dolore ea do do. Incididunt nisi laborum ipsum Lorem eu do
-        proident sit excepteur fugiat incididunt ut. Voluptate enim irure ipsum
-        et labore do enim Lorem qui aliquip officia esse ipsum. Quis elit
-        consequat occaecat nostrud irure culpa dolore. Adipisicing reprehenderit
-        consectetur est do nulla minim quis esse veniam. Aute labore dolore
-        ipsum nulla laboris magna ea. Sit ea officia aliquip dolore eiusmod
-        occaecat incididunt occaecat. Consectetur et qui est nisi fugiat
-        adipisicing sint aliqua amet excepteur adipisicing. Excepteur ipsum in
-        laborum nisi excepteur eiusmod eiusmod culpa eu. Dolore ea Lorem sint
-        proident excepteur ullamco labore ex. Pariatur laboris ex duis sunt in
-        cupidatat adipisicing adipisicing aliqua culpa mollit duis. Consectetur
-        nisi incididunt id Lorem pariatur occaecat adipisicing adipisicing duis
-        labore ex nulla voluptate sunt. Excepteur nostrud exercitation enim
-        officia amet excepteur ullamco labore exercitation pariatur. Fugiat
-        magna laborum dolor officia. Laboris ad quis cillum aliqua aliqua quis
-        dolore. Ipsum velit aute dolor enim adipisicing magna culpa ipsum minim
-        dolor elit velit ullamco. Culpa ex cillum non ut est. Minim nisi sint
-        aliquip fugiat adipisicing. Excepteur pariatur incididunt non aliqua
-        irure fugiat sunt aute. Ex deserunt cillum cupidatat ad. Eu non aute
-        nostrud reprehenderit velit adipisicing ex culpa qui. Quis dolore tempor
-        ipsum sint ex est aliquip ullamco. Adipisicing non eu dolore irure quis
-        nostrud nostrud labore esse in anim esse.</div>
+       <p> Ownership
+<br/>
+Sony India Private Limited maintains this Site (the “www.sony.co.in”) for your entertainment, information, education, and communication. Please feel free to browse the Site.
+<br/>
+If you are browsing the Site as a non-commercial consumer, you may download material displayed on the Site for your non-commercial, personal use only. If you are browsing this Site as an employee/agent/member of any business or organization, you may download material/ information displayed on the Site only for non-commercial purposes within your business or organization.
+<br/>
+This permission is specifically conditioned on your retaining all copyright and other proprietary notices contained on the materials. Additionally, you may not distribute or transmit (except, within your business or organization), modify, reuse, report, or use the contents of this Site for public or commercial purpose, including the text, images, audio and video without Sony’s written permission.
+<br/>
+Your access and use of the Site is also subject to the following terms and conditions (“Terms and Conditions”) and all applicable laws. By accessing and browsing the Site, you accept, without limitation or qualification, the Terms and Conditions below.
+<br/>
+Terms and Conditions of Use
+<br/>
+Prices and specifications are subject to change without prior notice. You should assume that all material / information on the Site is protected by copyright unless otherwise noted and may not be used except as permitted in these Terms and Conditions or in the text on this Site.
+<br/>
+The MRPs are inclusive of all taxes
+<br/>
+Limitation of Liability
+<br/>
+Although care has been taken to ensure the accuracy of the contents of this Site but Sony makes no warranties or representations as to its accuracy and Sony specifically disclaims any liability or responsibility for any errors or omissions in the content on the Site. Neither Sony nor any other party involved in creating, producing, or delivering the Site is liable for any direct, incidental, consequential, indirect, or punitive damages arising out of you access to, or use of, or inability to use or access, the Site. Without limiting the foregoing, everything on this Site is provided to you “AS IS” WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, OR NON-INFRINGEMENT.
+<br/>
+Please note that some jurisdictions may not allow the exclusion of implied warranties, so some of the above exclusions may not apply to you. Check your local laws for any restrictions or limitations regarding the exclusion of implied warranties. Features and specifications of products described or depicted on this Site are subject to change at any time without Notice. Non–metric weights and measures are approximate. All monitor screens are measured diagonally; all monitor screen images contained in this database are simulated pictures unless otherwise indicated. Sony assumes no responsibility, and shall not be liable for, any damages to, or viruses that may infect, your Computer equipment or other property on account of your access to, use of, or browsing in the Site or your downloading of any material, data, text, images, video or audio from the Site.
+<br/>
+Responsibility for User’s material/ information
+<br/>
+The material / information provided on the Site is either the property of, or used with permission by, Sony. Unless otherwise specified by these terms and conditions or specific permission provided elsewhere on this Site, the Sony Site are for your personal and non–commercial use. You may not distribute, exchange, modify, sell or transmit anything you copy from this Site, including but not limited to any text, images, audio and video, for any business, commercial or public purpose. As long as you comply with the terms and conditions of Use, Sony grants you a non-exclusive, non-transferable, limited right to enter, display and use this Site. Any unauthorized use of the images may violate copyright laws, trademark laws, the laws of privacy and publicity, and civil and criminal statues.
+<br/>
+Links
+<br/>
+The Site may provide links to other Web Sites, which are not under control of Sony. Sony shall not be responsible in any way for the contents of any such web Sites linked to or from the Sony India site. Any inclusion of such links shall not imply / interpreted as an endorsement of the owner / sponsor of the site or the content of the site. Sony disclaims all warranties, express and implied as to accuracy, validity, and legality or otherwise of any materials or information contained on such sites.
+<br/>
+Availability of Products and Services
+<br/>
+The products and services displayed on the Site may not be available for purchase in your particular locality. The reference to such products and services on the Site does not imply or warrant that these products or services will be available at any time in your particular location. You should check with your local Sony authorised representative for the availability of specific products and services in your area.
+<br/>
+Sony’s Right to Change These Terms and Conditions of Use or Content on the Site.
+<br/>
+Sony may at any time revise these Terms and Conditions by updating this posting. Since you are bound by these Terms and Conditions, you should therefore periodically visit this page, by clicking the Legal and Trademark hyperlink at the bottom of the Sony Web pages, to review the then current Terms and Conditions.
+<br/>
+These Terms and Conditions are to be governed by the construed in accordance with the law of India, without reference to its conflicts of law rules.
+<br/>
+Copyright
+<br/>
+Unless otherwise stated, copyright or similar rights in all material presented on the site, including graphical images, is owned by Sony India.
+<br/>
+Trademarks
+<br/>
+The Trademarks, logos and service marks (collectively the “Trademarks”) are the trademarks of Sony and / or its affiliates. Any other trademarks used in the Site are trademarks of their respective owners. Nothing contained on the Site should be construed as granting, by implication, or otherwise, any license or right of use any Trademark displayed on the Site without written permission of Sony.</p></div>
       </div>
       <Footer />
     </div>
