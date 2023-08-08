d0dd839ed31cfc8c150b3a53407f5c63e88e153a
utkarsh-gaur <utkarshgaur6014@gmail.com>
2023-08-08 16:58:57 +0530

    some minor changes

--- client/src/components/News.jsx
+++ client/src/components/News.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import { Box, Button, Input, VStack } from '@chakra-ui/react';
 import '../styles/news.css';
-import {MdKeyboardBackspace} from 'react-icons/md'
-import {Link} from 'react-router-dom'
+import { MdKeyboardBackspace } from 'react-icons/md'
+import { Link } from 'react-router-dom'
 
 const signup = () => {
   return (
     <div className="newspage" id='newsletter'>
-       <div className='back backbtn' > 
-  <Link to={'/'}>
-  <MdKeyboardBackspace size={30}/>
-  </Link>
-  </div> 
+      <div className='back backbtn' >
+        <Link to={'/'}>
+          <MdKeyboardBackspace size={30} />
+        </Link>
+      </div>
       <div className="news">
-        <VStack 
+        <VStack
           className='newsletterContent'
           spacing={'1.5'}
           w={'72'}
@@ -45,6 +45,10 @@ const signup = () => {
             type="tel "
             colorScheme="facebook"
           />
+          <div className='checkboxText'>
+          <input value="test" type="checkbox" /><br />
+            I agree to receive emails from Zinikus pvt ltd. <br />
+            View our <a href="/TermsConditions" style={{color:"Blue"}}>terms of service.</a></div>
           <Button colorScheme="twitter" m={'10'} variant={'solid'}>
             Subscribe
           </Button>
