69e7fa62f28403d7753a02ff307c26d9fc6b8a0f
utkarsh-gaur <utkarshgaur6014@gmail.com>
2023-08-10 13:39:49 +0530

    connected the feedback and newsletter to my firebase

--- client/src/components/Feedback.jsx
+++ client/src/components/Feedback.jsx
@@ -7,13 +7,61 @@ import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
 const Feedback = () => {
-  
+
   const [rating, setRating] = useState(0);
 
   // Function to update the rating and store it in local storage
   const handleStarClick = (selectedRating) => {
     setRating(selectedRating);
     localStorage.setItem('rating', selectedRating);
+  };
+
+  //uploading the data to firebase
+
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    opinions: "",
+    suggestions: "",
+  });
+  let name, value;
+  const getUserData = (event) => {
+    name = event.target.name;
+    value = event.target.value;
+
+    setUser({ ...user, [name]: value });
+  };
+
+  const postData = async (e) => {
+    e.preventDefault();
+
+    const { name, email,opinions,suggestions } = user;
+
+    const res = await fetch(
+      "https://testing-55e7d-default-rtdb.firebaseio.com/praticeFeedback.json"
+      , {
+        method: "POST",
+        headers: {
+          "Content-Type": "app/json",
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          opinions,
+          suggestions,
+        }),
+      }
+    );
+    if (res) {
+      setUser({
+        name: "",
+        email: "",
+        opinions: "",
+        suggestions:"",
+      });
+      alert("Data Stored Successfully!");
+
+    }
   };
   return (
     <div className="backgroundimg">
@@ -22,75 +70,89 @@ const Feedback = () => {
           <MdKeyboardBackspace size={30} />
         </Link>
       </div>
-      <div className="login" 
+      <div className="login"
       // style={{ marginBottom: '30rem' }}
       >
-        <VStack
-          className='feedbackContent'
-          spacing={'1.5'}
-          w={'72'}
-          pos={'absolute'}
-          marginBlock={'2rem'}
-          left={['40vw']}
-          top={'6rem'}
-          justifyContent={'center'}
-        >
-          <h2 className='fh2'>Feedback</h2>
-          <Input
-            variant="flushed"
-            placeholder="Name"
-            type="text"
-            colorScheme="facebook"
-            required
-          />
-          <Input
-            variant="flushed"
-            placeholder="Email Id"
-            type="email"
-            colorScheme="facebook"
-            required
-          />
-          <Input
-            variant="flushed"
-            placeholder="What did you like the best?"
-            type='text'
-            colorScheme="facebook"
-            required
-          />
-          <Input
-            variant="flushed"
-            placeholder="How can we improve?"
-            type='textbox'
-            colorScheme="facebook"
-            required
-          />
+        <form className='feedbackForm' method='POST'>
+          <VStack
+            className='feedbackContent'
+            spacing={'1.5'}
+            w={'72'}
+            pos={'absolute'}
+            marginBlock={'2rem'}
+            left={['40vw']}
+            top={'6rem'}
+            justifyContent={'center'}
+          >
+            <h2 className='fh2'>Feedback</h2>
+            <Input
+              variant="flushed"
+              name='name'
+              placeholder="Name"
+              value={user.name}
+              onChange={getUserData}
+              type="text"
+              colorScheme="facebook"
+              required
+            />
+            <Input
+              variant="flushed"
+              name='email'
+              placeholder="Email Id"
+              value={user.email}
+              onChange={getUserData}
+              type="email"
+              colorScheme="facebook"
+              required
+            />
+            <Input
+              variant="flushed"
+              name='opinions'
+              placeholder="What did you like the best?"
+              value={user.opinions}
+              onChange={getUserData}
+              type='text'
+              colorScheme="facebook"
+              required
+            />
+            <Input
+              variant="flushed"
+              name='suggestions'
+              placeholder="How can we improve?"
+              value={user.suggestions}
+              onChange={getUserData}
+              type='textbox'
+              colorScheme="facebook"
+              required
+            />
 
-<div>
-      {[...Array(5)].map((_, index) => (
-        <span
-          key={index}
-          style={{
-            cursor: 'pointer',
-            fontSize: '24px',
-            color: index < rating ? 'gold' : 'black',
-          }}
-          onClick={() => handleStarClick(index + 1)}
-        >
-          ★
-        </span>
-      ))}
-    </div>
+            <div>
+              {[...Array(5)].map((_, index) => (
+                <span
+                  key={index}
+                  style={{
+                    cursor: 'pointer',
+                    fontSize: '24px',
+                    color: index < rating ? 'gold' : 'black',
+                  }}
+                  onClick={() => handleStarClick(index + 1)}
+                >
+                  ★
+                </span>
+              ))}
+            </div>
 
 
-          <Button colorScheme="twitter" m={'10px'} variant={'solid'}>
-            Submit
-          </Button>
-        </VStack>
+            <Button colorScheme="twitter" m={'10px'} variant={'solid'} onClick={postData}>
+              Submit
+            </Button>
+          </VStack>
+        </form>
       </div>
     </div>
   );
 };
 
 
-export default Feedback
+export default Feedback;
 
--- client/src/components/News.jsx
+++ client/src/components/News.jsx
@@ -1,10 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box, Button, Input, VStack } from '@chakra-ui/react';
 import '../styles/news.css';
 import { MdKeyboardBackspace } from 'react-icons/md'
 import { Link } from 'react-router-dom'
 
 const signup = () => {
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    phone: "",
+  });
+  let name, value;
+  const getUserData = (event) => {
+    name = event.target.name;
+    value = event.target.value;
+
+    setUser({ ...user, [name]: value });
+  };
+
+  const postData = async (e) => {
+    e.preventDefault();
+
+    const{name,email,phone} = user;
+
+    const res = await fetch(
+      "https://testing-55e7d-default-rtdb.firebaseio.com/praticeNewsletter.json"
+      , {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          phone,
+        }),
+      }
+    );
+      if(res){
+        setUser({
+          name: "",
+          email: "",
+          phone: "",
+        });
+        alert("Data Stored Successfully!");
+
+      }
+  };
+
   return (
     <div className="newspage" id='newsletter'>
       <div className='back backbtn' >
@@ -13,46 +56,57 @@ const signup = () => {
         </Link>
       </div>
       <div className="news">
-        <VStack
-          className='newsletterContent'
-          spacing={'1.5'}
-          w={'72'}
-          pos={'absolute'}
-          left={['40%']}
-          top={'44'}
-        >
-          <label className="logintext">Subscribe to our Newsletter </label>
-          {/* <label className="loginlabel">Name</label> */}
-          <Input
-            variant="flushed"
-            placeholder="Name*"
-            type="text"
-            colorScheme="facebook"
-            required
-          />
-          {/* <label className="loginlabel">EmailID</label> */}
-          <Input
-            variant="flushed"
-            placeholder="E-mail*"
-            type="email"
-            colorScheme="facebook"
-            required
-          />
-          {/* <label className="loginlabel">Mobile number</label> */}
-          <Input
-            variant="flushed"
-            placeholder="Mobile Number"
-            type="tel "
-            colorScheme="facebook"
-          />
-          <div className='checkboxText'>
-          <input value="test" type="checkbox" />
-            I agree to receive emails from Zinikus pvt ltd.
-            View our <a href="/TermsConditions" style={{color:"Blue"}}>terms of service.</a></div>
-          <Button colorScheme="twitter" m={'10'} variant={'solid'}>
-            Subscribe
-          </Button>
-        </VStack>
+        <form className='newsletterForm' method='POST'>
+          <VStack
+            className='newsletterContent'
+            spacing={'1.5'}
+            w={'72'}
+            pos={'absolute'}
+            left={['40%']}
+            top={'44'}
+          >
+            <label className="logintext">Subscribe to our Newsletter </label>
+            {/* <label className="loginlabel">Name</label> */}
+            <Input
+              variant="flushed"
+              name='name'
+              placeholder="Name*"
+              value={user.name}
+              onChange={getUserData}
+              type="text"
+              colorScheme="facebook"
+              required
+            />
+            {/* <label className="loginlabel">EmailID</label> */}
+            <Input
+              variant="flushed"
+              name='email'
+              placeholder="E-mail*"
+              value={user.email}
+              onChange={getUserData}
+              type="email"
+              colorScheme="facebook"
+              required
+            />
+            {/* <label className="loginlabel">Mobile number</label> */}
+            <Input
+              variant="flushed"
+              name='phone'
+              placeholder="Mobile Number"
+              value={user.phone}
+              onChange={getUserData}
+              type="tel "
+              colorScheme="facebook"
+            />
+            <div className='checkboxText'>
+              <input value="test" type="checkbox" />
+              I agree to receive emails from Zinikus pvt ltd.
+              View our <a href="/TermsConditions" style={{ color: "Blue" }}>terms of service.</a></div>
+            <Button colorScheme="twitter" m={'10'} variant={'solid'} onClick={postData}>
+              Subscribe
+            </Button>
+          </VStack>
+        </form>
       </div>
     </div>
   );
