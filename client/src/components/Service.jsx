141fddba358f6976cc8ea4e69ff0ee83d3b21dea
utkarsh-gaur <utkarshgaur6014@gmail.com>
2023-08-17 16:38:12 +0530

    several changes

--- client/src/components/Service.jsx
+++ client/src/components/Service.jsx
@@ -6,22 +6,26 @@ import '../styles/services.css';
 const Service = () => {
   return (
     <div className="services">
-    <Header/>
+      <Header />
       <div className="callingLady">
-      <div className="gradC1"></div>
-          <div className="gradE1"></div>
+        <div className="gradC1"></div>
+        <div className="gradE1"></div>
         <div className="servicesGlassyEffect"></div>
         <div className="callHeading">
           <h1>How can we help?</h1>
           <p>
-            <span style={{ wordSpacing: '2rem' }}>Scroll for</span> more
+            <span style={{ wordSpacing: '0.5rem', display: 'flex' }}>Scroll <div class='mouse-container'>
+              <div class='mouse'>
+                <span class='scroll-down'></span>
+              </div>
+            </div> for more</span>
           </p>
         </div>
       </div>
       <div className="ourServices">
         <h1>Our Services</h1>
         <div className="gradC1"></div>
-          <div className="gradE1"></div>
+        <div className="gradE1"></div>
         <div className="serviceContainer">
           <div className="el1"></div>
           <div className="el2">
@@ -52,10 +56,10 @@ const Service = () => {
           <div className="el4"></div>
         </div>
       </div>
-      
+
       <div className="ContactspaceMaker"></div>
       <div className="gradC1"></div>
-          <div className="gradE1" style={{maxHeight: '400px'}}></div>
+      <div className="gradE1" style={{ maxHeight: '400px' }}></div>
       <Footer />
     </div>
   );
