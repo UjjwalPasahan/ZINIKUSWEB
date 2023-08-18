15d8215ebd98b1b1aba87ed29513504bcd8663fa
utkarsh-gaur <utkarshgaur6014@gmail.com>
2023-08-18 18:08:23 +0530

    fixes to the feedback form

--- client/src/components/Feedback.jsx
+++ client/src/components/Feedback.jsx
@@ -10,49 +10,119 @@ const Feedback = () => {
   const [rating, setRating] = useState(0);
 
   // Function to update the rating and store it in local storage
-  const handleStarClick = (selectedRating) => {
+  const handleStarClick = selectedRating => {
     setRating(selectedRating);
     localStorage.setItem('rating', selectedRating);
   };
 
   const [form, setForm] = useState({});
-  const handleForm = (e) => {
+  const handleForm = e => {
     console.log(e.target.value, e.target.name);
     setForm({
       ...form,
-      [e.target.name]: e.target.value
-    })
+      [e.target.name]: e.target.value,
+    });
   };
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = async e => {
     e.preventDefault();
-    const response = await fetch("http://localhost:8080/zinikusFeedback", {
-      method: 'POST',
-      body: JSON.stringify(form),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
+    try {
+      const response = await fetch('http://localhost:8080/zinikusFeedback', {
+        method: 'POST',
+        body: JSON.stringify(form),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
 
-    const data = await response.json();
-    console.log(data);
+      const data = await response.json();
+      console.log(data);
 
-    // Clear form fields after successful submission
-    setForm({});
+      // Clear form fields after successful submission
+      setForm({});
+      // Show an alert after data is stored successfully
+      alert('Data stored successfully');
+    } catch (error) {
+      console.error('Error submitting form:', error);
+    }
+  };
 
-    // Show an alert after data is stored successfully
-    alert("Data stored successfully");
-  }
   const getUsers = async () => {
-    const response = await fetch("http://localhost:8080/zinikusFeddback", {
-      method: 'GET'
-    })
-    const data = await response.json();
-    console.log(data);
-  }
+    try {
+      const response = await fetch('http://localhost:8080/zinikusFeedback', {
+        method: 'GET',
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error('Error fetching users:', error);
+    }
+  };
+
   useEffect(() => {
     getUsers();
   }, []);
+
+  // const [form, setForm] = useState({}); // Object to store form data
+
+  // const handleForm = e => {
+  //   console.log(e.target.value, e.target.name);
+  //   form[e.target.name] = e.target.value;
+  // };
+
+  // const handleSubmit = async e => {
+  //   e.preventDefault();
+  //   try {
+  //     const response = await fetch('http://localhost:8080/zinikusFeedback', {
+  //       method: 'POST',
+  //       body: JSON.stringify(form),
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //       },
+  //     });
+
+  //     const data = await response.json();
+  //     console.log(data);
+
+  //     // Clear form fields after successful submission
+  //     for (const key in form) {
+  //       if (form.hasOwnProperty(key)) {
+  //         form[key] = '';
+  //       }
+  //     }
+
+  //     // Show an alert after data is stored successfully
+  //     alert('Data stored successfully');
+  //   } catch (error) {
+  //     console.error('Error submitting form:', error);
+  //   }
+  // };
+
+  // const getUsers = async () => {
+  //   try {
+  //     const response = await fetch('http://localhost:8080/zinikusFeedback', {
+  //       method: 'GET',
+  //     });
+
+  //     if (!response.ok) {
+  //       throw new Error(`HTTP error! Status: ${response.status}`);
+  //     }
+
+  //     const data = await response.json();
+  //     console.log(data);
+  //   } catch (error) {
+  //     console.error('Error fetching users:', error);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   getUsers();
+  // }, []);
 
   return (
     <div className="backgroundimg">
@@ -61,12 +131,13 @@ const Feedback = () => {
           <MdKeyboardBackspace size={30} />
         </Link>
       </div>
-      <div className="login"
-      // style={{ marginBottom: '30rem' }}
+      <div
+        className="login"
+        // style={{ marginBottom: '30rem' }}
       >
-        <form className='feedbackForm' onSubmit={handleSubmit}>
+        <form className="feedbackForm" onSubmit={handleSubmit}>
           <VStack
-            className='feedbackContent'
+            className="feedbackContent"
             spacing={'1.5'}
             w={'72'}
             pos={'absolute'}
@@ -75,10 +146,10 @@ const Feedback = () => {
             top={'6rem'}
             justifyContent={'center'}
           >
-            <h2 className='fh2'>Feedback</h2>
+            <h2 className="fh2">Feedback</h2>
             <Input
               variant="flushed"
-              name='name'
+              name="name"
               placeholder="Name"
               value={form.name || ''}
               onChange={handleForm}
@@ -88,7 +159,7 @@ const Feedback = () => {
             />
             <Input
               variant="flushed"
-              name='email'
+              name="email"
               placeholder="Email Id"
               value={form.email || ''}
               onChange={handleForm}
@@ -98,21 +169,21 @@ const Feedback = () => {
             />
             <Input
               variant="flushed"
-              name='opinions'
+              name="opinions"
               placeholder="What did you like the best?"
               value={form.opinions || ''}
               onChange={handleForm}
-              type='text'
+              type="text"
               colorScheme="facebook"
               required
             />
             <Input
               variant="flushed"
-              name='suggestions'
+              name="suggestions"
               placeholder="How can we improve?"
               value={form.suggestions || ''}
               onChange={handleForm}
-              type='textbox'
+              type="textbox"
               colorScheme="facebook"
               required
             />
@@ -133,8 +204,12 @@ const Feedback = () => {
               ))}
             </div>
 
-
-            <Button type='submit' colorScheme="twitter" m={'10px'} variant={'solid'}>
+            <Button
+              type="submit"
+              colorScheme="twitter"
+              m={'10px'}
+              variant={'solid'}
+            >
               Submit
             </Button>
           </VStack>
@@ -144,6 +219,4 @@ const Feedback = () => {
   );
 };
 
-
 export default Feedback;
-
