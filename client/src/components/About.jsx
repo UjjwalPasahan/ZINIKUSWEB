780899e2d8648d48e505c2a30523f21385966d5a
utkarsh-gaur <utkarshgaur6014@gmail.com>
2023-08-24 23:34:45 +0530

    fixes to the orientation

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,13 +1,9 @@
-import React,{useRef,useEffect} from 'react';
-import {
-  Image,
-  HStack,
-  Button,
-} from '@chakra-ui/react';
+import React, { useRef, useEffect } from 'react';
+import { Image, HStack, Button } from '@chakra-ui/react';
 import img4 from '../assets/homepage/bg.png';
 import '../styles/about.css';
 import img5 from '../assets/homepage/dubai3.png';
-import "react-responsive-carousel/lib/styles/carousel.min.css";
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import { Carousel } from 'react-responsive-carousel';
 import { Link } from 'react-router-dom';
 
@@ -17,19 +13,19 @@ const About = () => {
   //     const s1 = entry.target.querySelector('.s1');
   //     const s2 = entry.target.querySelector('.s2');
   //     const s3 = entry.target.querySelector('.s3');
-  
+
   //     if (entry.isIntersecting) {
   //       s1.classList.add('faltuHeading-animation');
   //       s2.classList.add('faltuText-animation');
   //       s3.classList.add('imgs-animation');
   //     return; // if we added the className, exit the function
   //     }
-  
+
   //     // We're not intersecting, so remove the className!
   //     // square.classList.remove('square-animation');
   //   });
   // });
-  
+
   // observer.observe(document.querySelector('.lowerAboutUs'));
   const observerRef = useRef(null);
 
@@ -73,20 +69,34 @@ const About = () => {
 
   return (
     <div className="about" id="about">
-      <div className='upperAbout'>
-      <div className='lowerSpaceMaker' ></div>
-      {/* <div className='abouth2'><h2>ABOUT US</h2></div> */}
-      <div className='h22'><h2>ABOUT US</h2></div>
-      <div className='lowerSpaceMaker' ></div>
-      <div><p className='aboutp' >
-      Finding Inspiration in Every Turn
-<br/>
-We are dedicated to creating cutting-edge technology that pushes the boundaries of what's possible in the world of automation. Our team is composed of experts in various fields, including robotics, machine learning, computer science, and engineering. Together, we're creating a future where robots can perform complex tasks with ease, making our lives easier and more efficient.
-
-<br/>
-
-At our company, we believe in constant innovation and collaboration. We're always looking for new ways to improve our products and services and are open to working with partners who share our vision. We're excited to be at the forefront of this exciting field and look forward to pushing the boundaries of what's possible in the world of A.I. and robotics.
-      </p></div>
+      <div className="upperAbout">
+        <div className="whiteLowerSpaceMaker"></div>
+        {/* <div className='abouth2'><h2>ABOUT US</h2></div> */}
+        <div className="h22">
+          <h2>
+            <a href="">ABOUT US</a>
+          </h2>
+        </div>
+        <div className="whiteLowerSpaceMaker"></div>
+        <div>
+          <p className="aboutp">
+            Finding Inspiration in Every Turn
+            <br />
+            We are dedicated to creating cutting-edge technology that pushes the
+            boundaries of what's possible in the world of automation. Our team
+            is composed of experts in various fields, including robotics,
+            machine learning, computer science, and engineering. Together, we're
+            creating a future where robots can perform complex tasks with ease,
+            making our lives easier and more efficient.
+            <br />
+            At our company, we believe in constant innovation and collaboration.
+            We're always looking for new ways to improve our products and
+            services and are open to working with partners who share our vision.
+            We're excited to be at the forefront of this exciting field and look
+            forward to pushing the boundaries of what's possible in the world of
+            A.I. and robotics.
+          </p>
+        </div>
       </div>
 
       <div className="midaboutus">
