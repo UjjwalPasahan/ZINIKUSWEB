13bdda8655a4fff943f36d474b209f8298be54e1
utkarsh-gaur <utkarshgaur6014@gmail.com>
2023-08-25 12:20:01 +0530

    detailing work

--- client/src/components/News.jsx
+++ client/src/components/News.jsx
@@ -1,28 +1,27 @@
 import React, { useEffect, useState } from 'react';
 import { Box, Button, Input, VStack } from '@chakra-ui/react';
 import '../styles/news.css';
-import { MdKeyboardBackspace } from 'react-icons/md'
-import { Link } from 'react-router-dom'
+import { MdKeyboardBackspace } from 'react-icons/md';
+import { Link } from 'react-router-dom';
 
 const signup = () => {
-
   const [form, setForm] = useState({});
-  const handleForm = (e) => {
+  const handleForm = e => {
     console.log(e.target.value, e.target.name);
     setForm({
       ...form,
-      [e.target.name]: e.target.value
-    })
+      [e.target.name]: e.target.value,
+    });
   };
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = async e => {
     e.preventDefault();
-    const response = await fetch("http://localhost:8080/zinikus", {
+    const response = await fetch('http://localhost:8080/zinikus', {
       method: 'POST',
       body: JSON.stringify(form),
       headers: {
-        'Content-Type': 'application/json'
-      }
+        'Content-Type': 'application/json',
+      },
     });
 
     const data = await response.json();
@@ -32,30 +31,30 @@ const signup = () => {
     setForm({});
 
     // Show an alert after data is stored successfully
-    alert("Data stored successfully");
-  }
+    alert('Data stored successfully');
+  };
   const getUsers = async () => {
-    const response = await fetch("http://localhost:8080/zinikus", {
-      method: 'GET'
-    })
+    const response = await fetch('http://localhost:8080/zinikus', {
+      method: 'GET',
+    });
     const data = await response.json();
     console.log(data);
-  }
+  };
   useEffect(() => {
     getUsers();
   }, []);
 
   return (
-    <div className="newspage" id='newsletter'>
-      <div className='back backbtn' >
+    <div className="newspage" id="newsletter">
+      <div className="back backbtn">
         <Link to={'/'}>
           <MdKeyboardBackspace size={30} />
         </Link>
       </div>
       <div className="news">
-        <form className='newsletterForm' onSubmit={handleSubmit}>
+        <form className="newsletterForm" onSubmit={handleSubmit}>
           <VStack
-            className='newsletterContent'
+            className="newsletterContent"
             spacing={'1.5'}
             w={'72'}
             pos={'absolute'}
@@ -66,7 +65,7 @@ const signup = () => {
             {/* <label className="loginlabel">Name</label> */}
             <Input
               variant="flushed"
-              name='name'
+              name="name"
               placeholder="Name*"
               value={form.name || ''}
               onChange={handleForm}
@@ -77,7 +76,7 @@ const signup = () => {
             {/* <label className="loginlabel">EmailID</label> */}
             <Input
               variant="flushed"
-              name='email'
+              name="email"
               placeholder="E-mail*"
               value={form.email || ''}
               onChange={handleForm}
@@ -88,18 +87,32 @@ const signup = () => {
             {/* <label className="loginlabel">Mobile number</label> */}
             <Input
               variant="flushed"
-              name='phone'
+              name="phone"
               placeholder="Mobile Number"
               value={form.phone || ''}
               onChange={handleForm}
               type="tel "
               colorScheme="facebook"
             />
-            <div className='checkboxText'>
-              <input value="test" type="checkbox" />
-              I agree to receive emails from Zinikus pvt ltd.
-              View our <a href="/TermsConditions" style={{ color: "Blue" }}>terms of service.</a></div>
-            <Button type='submit' colorScheme="twitter" m={'10'} variant={'solid'}>
+            <div className="checkboxText">
+              <input
+                name="tick"
+                type="checkbox"
+                value={form.checkbox || 'false'}
+                onChange={handleForm}
+                style={{ marginRight: '2px' }}
+              />
+              I agree to receive emails from Zinikus pvt ltd. View our{' '}
+              <a href="/TermsConditions" style={{ color: '#251b58' }}>
+                terms of service.
+              </a>
+            </div>
+            <Button
+              type="submit"
+              colorScheme="twitter"
+              m={'10'}
+              variant={'solid'}
+            >
               Subscribe
             </Button>
           </VStack>
