750ec81b1a2eac64db9bfc2d4ea3cbcbf407de0e
UjjwalPasahan <96993068+UjjwalPasahan@users.noreply.github.com>
2023-08-26 15:41:06 +0530

    button 404

--- client/src/components/Error.jsx
+++ client/src/components/Error.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import '../styles/error.css';
+import {Button} from '@chakra-ui/react'
 
 const Error = () => {
     return (
-        <div className='error'>
+        <div className='error' style={{display:"flex",flexDirection:"column",alignItems:"center",backgroundColor:"white",height:"100vh"}}>
             <div className="page_404">
                 <h2 className='errorHeading'>404.</h2>
+            
             </div>
+            <button className="headaboutbtn" style={{marginBottom:"5%"}}>
+              <a href={'/'}>Back Home</a>
+            </button>
         </div>
     );
 };
