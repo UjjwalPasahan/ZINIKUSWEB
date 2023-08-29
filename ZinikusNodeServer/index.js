e1face88db6cf8602ccf0cf1d41eec30766b0666
utkarsh-gaur <utkarshgaur6014@gmail.com>
2023-08-30 01:10:20 +0530

    mongodbAtlas

--- ZinikusNodeServer/index.js
+++ ZinikusNodeServer/index.js
@@ -7,7 +7,9 @@ const mongoose = require("mongoose");
 main().catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/zinikus");
+  await mongoose.connect(
+    "mongodb+srv://technologyzinikus:RlqAholaDWONzoRS@cluster0.byasjtz.mongodb.net/zinikus"
+  );
   console.log("db connected");
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
